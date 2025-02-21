9a76265f7c843820334b3ed0888f7c24b693d1b1
ReggieMW <reggiewoods@msn.com>
2025-02-21 16:55:55 -0600

    added school subjects program

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -29,6 +29,33 @@ namespace SelectionStatementExercise
                     Console.WriteLine("You guessed my favorite number, you win!");
                 }
             } while (correctGuess == false);
+            
+            
+            //user's favorite school subject
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+            
+            switch (favoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Mathematics knowledge can really come in handy with programming.");
+                    break;
+                case "science":
+                    Console.WriteLine("Scientist are great at using experiments and data to figure out how the world works, but no one listens to them.");
+                    break;
+                case "history":
+                    Console.WriteLine("Historians are very knowledgeable about how to fix society but no one listens to them.");
+                    break;
+                case "english":
+                    Console.WriteLine("English majors make great servers and bartenders!");
+                    break;
+                case "art":
+                    Console.WriteLine("You're a beautiful soul but AI is going to take your job so maybe you can teach?");
+                    break;
+                default:
+                    Console.WriteLine("Oooooo-kay, yeah, so have fun with that.");
+                    break;
+            }
         }
     }
 }
